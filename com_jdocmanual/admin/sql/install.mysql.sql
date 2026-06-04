adf728afb154310f475ff5d9dc90cab1f0263a3a
Cliff Ford <cliff@ford.myzen.co.uk>
2026-06-04 10:20:39 +0100

    Fix comma missing from  installation sql.

--- com_jdocmanual/admin/sql/install.mysql.sql
+++ com_jdocmanual/admin/sql/install.mysql.sql
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `#__jdm_article_stashes` (
 CREATE TABLE IF NOT EXISTS `#__jdm_languages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lang_id` int(11) NOT NULL,
-  `state` int(11) NOT NULL
+  `state` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
